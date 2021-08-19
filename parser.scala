e0caa3bc98dc2c2ec8a26c29181e875c908317d8
Nada Amin <namin@seas.harvard.edu>
2021-08-20 00:44:14 +0200

    actual packrat parsers

--- parser.scala
+++ parser.scala
@@ -36,65 +36,66 @@ class FamParser extends RegexParsers with PackratParsers {
   def constructor_name: Parser[String] = """[A-Z][a-z]*""".r ^^ { _.toString }
 
   // FAMILY PATHS
-  def fampath : Parser[FamilyPath] =
+  lazy val fampath : PackratParser[FamilyPath] =
     family_name ^^ {s => AbsoluteFamily(Family(s))}
     | "self(" ~> family_name <~ ")" ^^ {s => SelfFamily(Family(s))}
 
   // TYPES
-  def funtype: Parser[FunType] = "(" ~> typ ~ "->" ~ typ <~ ")" ^^ { case inp~_~out => FunType(inp, out)}
-  def recfield: Parser[(String, Type)] = field_name ~ ":" ~ typ ^^ {case k~":"~v => k -> v}
-  def rectype: Parser[RecType] = "{"~> repsep(recfield, ",") <~"}" ^^ {case lst => RecType(lst.toMap)}
-  def famtype: Parser[FamType] = fampath ~ "." ~ type_name ^^ { case p~_~t => FamType(p, t)}
-  def ntype: Parser[Type] = "N" ^^ (_ => N)
-  def btype: Parser[Type] = "B" ^^ (_ => B)
+  lazy val funtype: PackratParser[FunType] = "(" ~> typ ~ "->" ~ typ <~ ")" ^^ { case inp~_~out => FunType(inp, out)}
+  lazy val recfield: PackratParser[(String, Type)] = field_name ~ ":" ~ typ ^^ {case k~":"~v => k -> v}
+  lazy val rectype: PackratParser[RecType] = "{"~> repsep(recfield, ",") <~"}" ^^ {case lst => RecType(lst.toMap)}
+  lazy val famtype: PackratParser[FamType] = fampath ~ "." ~ type_name ^^ { case p~_~t => FamType(p, t)}
+  lazy val ntype: PackratParser[Type] = "N" ^^ (_ => N)
+  lazy val btype: PackratParser[Type] = "B" ^^ (_ => B)
 
-  def typ: Parser[Type] = funtype | rectype | famtype | ntype | btype | "(" ~> typ <~ ")"
+  lazy val typ: PackratParser[Type] = funtype | rectype | famtype | ntype | btype | "(" ~> typ <~ ")"
 
   // ADTS
-  def adt_constructor: Parser[(String, RecType)] = constructor_name ~ rectype ^^ {case k ~ v => k -> v}
-  def adt: Parser[ADT] = repsep(adt_constructor, "|") ^^ {case lst => ADT(lst.toMap)}
+  lazy val adt_constructor: PackratParser[(String, RecType)] = constructor_name ~ rectype ^^ {case k ~ v => k -> v}
+  lazy val adt: PackratParser[ADT] = repsep(adt_constructor, "|") ^^ {case lst => ADT(lst.toMap)}
 
   // EXPRESSIONS
-  def exp_bool_true: Parser[Bexp] = "true" ^^ {_ => Bexp(true)}
-  def exp_bool_false: Parser[Bexp] = "false" ^^ {_ => Bexp(false)}
-  def exp_nat: Parser[Nexp] = """(0|[1-9]\d*)""".r ^^ { n => Nexp(n.toInt) }
-  def exp_var: Parser[Var] = var_name ^^ {id => Var(id)}
-  def lam_input: Parser[(Var, Type)] = "(" ~> exp_var ~ ":" ~ typ <~ ")" ^^ {case v~":"~t => v -> t}
-  def exp_lam: Parser[Lam] =
+  lazy val exp_bool_true: PackratParser[Bexp] = "true" ^^ {_ => Bexp(true)}
+  lazy val exp_bool_false: PackratParser[Bexp] = "false" ^^ {_ => Bexp(false)}
+  lazy val exp_nat: PackratParser[Nexp] = """(0|[1-9]\d*)""".r ^^ { n => Nexp(n.toInt) }
+  lazy val exp_var: PackratParser[Var] = var_name ^^ {id => Var(id)}
+  lazy val lam_input: PackratParser[(Var, Type)] = "(" ~> exp_var ~ ":" ~ typ <~ ")" ^^ {case v~":"~t => v -> t}
+  lazy val exp_lam: PackratParser[Lam] =
     "lam" ~> lam_input ~ "." ~ exp ^^ {case inp~_~body => Lam(inp._1, inp._2, body)}
 
-  def exp_famfun: Parser[FamFun] = fampath ~ "." ~ function_name ^^ {case p~"."~n => FamFun(p, n)}
-  def exp_app: Parser[App] = exp ~ exp ^^ {case e~g => App(e, g)}
-  def exp_proj: Parser[Proj] = exp ~ "." ~ field_name ^^ {case e~_~n => Proj(e, n)}
-  def field_val: Parser[(String, Expression)] = field_name ~ "=" ~ exp ^^ {case k~":"~v => k -> v}
-  def exp_rec: Parser[Rec] = "{"~> repsep(field_val, ",") <~"}" ^^ {case lst => Rec(lst.toMap)}
-  def exp_inst: Parser[Inst] = famtype ~ "(" ~ exp_rec <~ ")" ^^ {case t~"("~r => Inst(t, r)}
-  def exp_inst_adt: Parser[InstADT] =
+  lazy val exp_famfun: PackratParser[FamFun] = fampath ~ "." ~ function_name ^^ {case p~"."~n => FamFun(p, n)}
+  lazy val exp_app: PackratParser[App] = exp ~ exp ^^ {case e~g => App(e, g)}
+  lazy val exp_proj: PackratParser[Proj] = exp ~ "." ~ field_name ^^ {case e~_~n => Proj(e, n)}
+  lazy val field_val: PackratParser[(String, Expression)] = field_name ~ "=" ~ exp ^^ {case k~":"~v => k -> v}
+  lazy val exp_rec: PackratParser[Rec] = "{"~> repsep(field_val, ",") <~"}" ^^ {case lst => Rec(lst.toMap)}
+  lazy val exp_inst: PackratParser[Inst] = famtype ~ "(" ~ exp_rec <~ ")" ^^ {case t~"("~r => Inst(t, r)}
+  lazy val exp_inst_adt: PackratParser[InstADT] =
     famtype ~ "(" ~ constructor_name ~ exp_rec <~ ")" ^^ {case t~"("~c~r => InstADT(t, c, r)}
 
-  def match_case: Parser[(String, Lam)] = constructor_name ~ "=>" ~ exp_lam ^^ {case k~"=>"~v => k -> v}
-  def exp_match: Parser[Match] =
+  lazy val match_case: PackratParser[(String, Lam)] = constructor_name ~ "=>" ~ exp_lam ^^ {case k~"=>"~v => k -> v}
+  lazy val exp_match: PackratParser[Match] =
     "match" ~> exp ~ "with" ~ repsep(match_case, "|") <~ "." ^^ {case e~_~lst => Match(e, lst.toMap)}
 
-  def exp: Parser[Expression] =
-    exp_lam | exp_famfun | exp_bool_true | exp_bool_false | exp_nat | exp_var
+  lazy val exp: PackratParser[Expression] =
+    exp_app | exp_proj | exp_famfun
+  | exp_lam | exp_bool_true | exp_bool_false | exp_nat
   | exp_match | exp_inst_adt | exp_inst | exp_rec
-  | exp_proj | exp_app
+  | exp_var
   | "(" ~> exp <~ ")"
 
   // MARKERS
-  def marker: Parser[Marker] =
+  lazy val marker: PackratParser[Marker] =
     "=" ^^ {_ => Eq} | "+=" ^^ {_ => PlusEq}
 
   // DEFINITIONS
-  def typedef: Parser[(String, (Marker, RecType))] =
+  lazy val typedef: PackratParser[(String, (Marker, RecType))] =
     "type" ~> type_name ~ marker ~ rectype ^^ {case n~m~rt => (n -> (m -> rt))}
-  def adtdef: Parser[(String, (Marker, ADT))] =
+  lazy val adtdef: PackratParser[(String, (Marker, ADT))] =
     "type" ~> type_name ~ marker ~ adt ^^ {case n~m~a => (n -> (m -> a))}
-  def fundef: Parser[(String, (FunType, Lam))] =
+  lazy val fundef: PackratParser[(String, (FunType, Lam))] =
     "val" ~> function_name ~ ":" ~ funtype ~ "=" ~ exp_lam ^^ {case m~":"~t~"="~b => m -> (t -> b)}
 
-  def famdef: Parser[Linkage] =
+  lazy val famdef: PackratParser[Linkage] =
     "Family" ~> family_name ~ "extends" ~ family_name ~ "{" ~ rep(typedef) ~ rep(adtdef) ~ rep(fundef) <~ "}" ^^
     {case a~_~b~_~typs~adts~funs =>
       Linkage(SelfFamily(Family(a)), SelfFamily(Family(b)), typs.toMap, adts.toMap, funs.toMap)}
@@ -103,7 +104,7 @@ class FamParser extends RegexParsers with PackratParsers {
 
 object TestFamParser extends FamParser {
   def main(args: Array[String]) = {
-    parse(exp, "lam (x : B). x") match {
+    parse(phrase(exp), "lam (x : B). x") match {
       case Success(matched,tail) => println(matched)
       case Failure(msg,tail) => println(s"FAILURE: $msg")
       case Error(msg,_) => println(s"ERROR: $msg")
