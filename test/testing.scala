d55b7b14d92638bb8367de10b2bea6c5d6008a9a
Nada Amin <namin@seas.harvard.edu>
2022-10-20 21:18:19 -0400

    one more

--- test/testing.scala
+++ test/testing.scala
@@ -1011,24 +1011,23 @@ class FamFunTesting extends AnyFunSuite {
     }
   }
 
-  /* TODO(now)
   test("typinf: type application of cases to a record") {
     val self_a = SelfFamily(Prog, "A")
     // self(A).R({f->true, n->5})
-    val exp = InstADT(FamType(self_a, "R"), "C", Rec(Map("f"->BConst(true), "n"->NConst(5))))
-
-    assertResult(Some(RecType(Map("C"->FunType(RecType(Map("f"->BType, "n"->NType)), NType))))){
-      typInf(App(FamCases(self_a, "cs"), Rec(Map())), Map(),
-        Map(self_a->
-          Linkage(self_a, null, Map(), Map(),
-            // list of ADTs has R = C {f:B, n:N}
-            Map("R"->(Eq, ADT(Map("C"->RecType(Map("f"->BType, "n"->NType)))))), Map(),
-            Map("cs"->(FamType(self_a, "R"), Eq, FunType(RecType(Map()), RecType(Map("C"->FunType(RecType(Map("f"->BType, "n"->NType)), NType)))),
-              Lam(Var("x"), RecType(Map()), Rec(Map("C" -> Lam(Var("r"), RecType(Map("f"->BType, "n"->NType)), NConst(1))))))))))
+    val exp = InstADT(FamType(Some(Sp(self_a)), "R"), "C", Rec(Map("f"->BConst(true), "n"->NConst(5))))
+    val k = Linkage(Sp(Prog), Prog, None, Map(), Map(), Map(), Map(), Map(),
+      Map("A" -> Linkage(AbsoluteFamily(Sp(Prog), "A"), self_a, None,
+        Map(), Map(),
+        Map("R"->(AdtDefn("R", Eq, DefnBody(Some(Map("C"->RecType(Map("f"->BType, "n"->NType)))), None, None)))),
+        Map(),
+        Map("cs"->(CasesDefn("cs", FamType(Some(Sp(self_a)), "R"), FunType(RecType(Map()), RecType(Map("C"->FunType(RecType(Map("f"->BType, "n"->NType)), NType)))), Eq,
+              DefnBody(Some(Lam(Var("x"), RecType(Map()), Rec(Map("C" -> Lam(Var("r"), RecType(Map("f"->BType, "n"->NType)), NConst(1)))))), None, None)))),
+        Map())))
+    initK(k)
+    assertResult(Right(RecType(Map("C"->FunType(RecType(Map("f"->BType, "n"->NType)), NType))))){
+      typInf(App(FamCases(Some(Sp(self_a)), "cs"), Rec(Map())))
     }
   }
-
-   */
 
   /* ==================================== LINKAGE TESTING ==================================== */
 
