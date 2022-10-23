946101b237b606d9fe865121bdf98b5613eb5e46
Nada Amin <namin@seas.harvard.edu>
2022-10-23 17:14:09 -0400

    Revert "another kludge to almost get there with mixins"
    This reverts commit c7cc21ebe979b63f85d89c714e4fb57d69b4d6ca.

--- src/code_generation.scala
+++ src/code_generation.scala
@@ -437,12 +437,7 @@ object code_generation {
       val translationTerm: String = ctorCalls.foldRight("from") { (c, r) =>
         s"$c($r)"
       }
-      var r = s"def $targetPathId$$$$${adtDefn.name}(from: $targetPathId.${adtDefn.name}): ${adtDefn.name} = $translationTerm"
-      // TODO(now): commented out because these don't compile, and they don't seem part of the interface?
-      if (curPath != targetPath && translationTerm == "from") {
-        r = "//"+r
-      }
-      r
+      s"def $targetPathId$$$$${adtDefn.name}(from: $targetPathId.${adtDefn.name}): ${adtDefn.name} = $translationTerm"
     }.mkString("\n")
   }
 
