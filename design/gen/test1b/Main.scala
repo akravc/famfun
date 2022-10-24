bc543dadea87dba215657ae97f0a5ccc3481eb69
Nada Amin <namin@seas.harvard.edu>
2022-10-23 21:20:19 -0400

    add a Main

---
+++ design/gen/test1b/Main.scala
@@ -0,0 +1,4 @@
+object Main extends App {
+  println(A$BB.Family.eval_cases(A$BB.ENat(1).asInstanceOf[A$BB.Family.self$.Exp])(()))
+  println(A$BB.Family.eval_cases(A$BB.A$AA$$AAA$$Exp(A$AA$AAA.EBase()).asInstanceOf[A$BB.Family.self$.Exp])(()))
+}
---
+++ design/gen/test1b/build.sbt
@@ -0,0 +1,5 @@
+lazy val root = (project in file("."))
+  .settings(
+    name := "famfun-design",
+    scalaVersion := "3.2.0",
+  )
