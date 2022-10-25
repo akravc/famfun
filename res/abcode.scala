e1ee308aa52c62131d384489478e8cba5b9852e5
Nada Amin <namin@seas.harvard.edu>
2022-10-25 15:28:25 -0400

    test files

---
+++ res/abcode.scala
@@ -0,0 +1,5 @@
+object ABApp {
+  def main(args: Array[String]): Unit = {
+    println(ABMain.Family.main(()))
+  }
+}
---
+++ res/abcodeover.scala
@@ -0,0 +1,6 @@
+object ABOverApp {
+  def main(args: Array[String]): Unit = {
+    println(ABOverMain.Family.main1(()))
+    println(ABOverMain.Family.main2(()))
+  }
+}
